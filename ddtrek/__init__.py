f3183fc315b3a635be5031664786ef41e084ac30
Evgenii Osipov <evgenii.osipov@kuleuven.be>
2024-10-23 16:35:08 +0200

    fix typo

--- ddtrek/__init__.py
+++ ddtrek/__init__.py
@@ -62,7 +62,7 @@ def make_dialog():
 
 
     # populate the Window from our *.ui file which was created with the Qt Designer
-    uifile = os.path.join(os.path.dirname(__file__), 'ddtrek_tabs.ui')
+    uifile = os.path.join(os.path.dirname(__file__), 'ddtrek.ui')
     form = loadUi(uifile, dialog)
 
 
